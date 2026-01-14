ba727721217b72e6abb7ff5be896ea1ea76cc30f
Subhan Khan Pathan <69514212+SubhanKhanPathan99@users.noreply.github.com>
2026-01-14 15:01:06 -0600

    UI: adwed

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ st.markdown("""
 # 3. SECURE API INITIALIZATION
 if "GEMINI_API_KEY" in st.secrets:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-flash-latest')
 else:
     st.error("Missing GEMINI_API_KEY in Secrets.")
     st.stop()
